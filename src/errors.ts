b7ec2ad3b5fbd847afb894d5e7725fb8c156af6e
Daniel Fey <feydanm@gmail.com>
2020-09-08 22:45:35 -0700

    formatting

--- src/errors.ts
+++ src/errors.ts
@@ -1,12 +1,16 @@
+type ExtraVars =
+  | string
+  | number
+  | Error
+  | {
+      [key: string]: string | number | Error | ExtraVars | ExtraVars[]
+      [key: number]: string | number | Error | ExtraVars | ExtraVars[]
+    }
 
-type ExtraVars = string | number | Error | {
-  [key: string]: string | number | Error | ExtraVars | ExtraVars[] 
-  [key: number]: string | number | Error | ExtraVars | ExtraVars[]
-}
-
-export interface AbstractErrorConstructor<L extends AbstractError> {
-  new (message?: string, extraVars?: ExtraVars): L
-}
+export type AbstractErrorConstructor<L extends AbstractError> = new (
+  message?: string,
+  extraVars?: ExtraVars
+) => L
 
 export abstract class AbstractError extends Error {
   public abstract readonly type: string
