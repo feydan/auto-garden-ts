c96e8c1c99588d2943162fe667f91dfd7a51495f
Daniel Fey <feydanm@gmail.com>
2021-05-20 00:04:14 -0700

    Adding logs

--- src/test/relay.ts
+++ src/test/relay.ts
@@ -1,11 +1,24 @@
+import { constVoid } from 'fp-ts/lib/function'
 import { pipe } from 'fp-ts/lib/pipeable'
-import * as TE from 'fp-ts/lib/TaskEither'
 import * as T from 'fp-ts/lib/Task'
+import * as TE from 'fp-ts/lib/TaskEither'
+import { observe } from 'src/tools/utils'
 import { gpio } from '../tools/raspberry-pi/gpio'
+
+const delay = (seconds: number) => () => T.delay(seconds * 1000)(TE.fromIO(constVoid))
 
 pipe(
   gpio(26, 'out'),
+
+  TE.map(observe(() => console.log('on'))),
   TE.chainFirstW(g => g.write(true)),
+  TE.chainFirstW(delay(2)),
+
+  TE.map(observe(() => console.log('off'))),
   TE.chainFirstW(g => T.delay(2)(g.write(false))),
+  TE.chainFirstW(delay(2)),
+  
+  TE.map(observe(() => console.log('destroy'))),
+  TE.chainFirstW(g => T.delay(2)(g.destroy())),
   TE.bimap(console.dir, console.dir)
 )()
