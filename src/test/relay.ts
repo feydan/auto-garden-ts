7ede5d3fd0eedad7ca318de05c68f911f4dbab65
Daniel Fey <feydanm@gmail.com>
2021-05-20 01:05:14 -0700

    adjusting widen chains

--- src/test/relay.ts
+++ src/test/relay.ts
@@ -5,18 +5,18 @@ import * as TE from 'fp-ts/lib/TaskEither'
 import { gpio } from '../tools/raspberry-pi/gpio'
 import { observe } from '../tools/utils'
 
-const delay = (seconds: number) => () => T.delay(seconds * 1000)(TE.fromIO(constVoid))
+const delay = <L>(seconds: number) => () => T.delay(seconds * 1000)(TE.fromIO<L, void>(constVoid))
 
 pipe(
   gpio(26, 'out'),
 
   TE.map(observe(() => console.log('on'))),
   TE.chainFirstW(g => g.write(true)),
-  TE.chainFirstW(delay(2)),
+  TE.chainFirst(delay(2)),
 
   TE.map(observe(() => console.log('off'))),
   TE.chainFirstW(g => g.write(false)),
-  TE.chainFirstW(delay(2)),
+  TE.chainFirst(delay(2)),
   
   TE.map(observe(() => console.log('destroy'))),
   TE.chainW(g => g.destroy()),
